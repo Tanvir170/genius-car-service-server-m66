3719ad17182740a69ac173a06bde0141dd691271
Your Name <you@example.com>
2022-04-28 16:17:44 +0600

    hero added

---
+++ __server_deploy.js
@@ -0,0 +1,28 @@
+/**
+ * ----------------
+ * ONE TIME FOR MY COMPUTER
+ * ----------------
+ * crete heroku account
+ * verify email
+ * install heroku cli
+ * heroku login
+ * -------------------------
+ * FOR EACH PROJECT ONE TIME
+ * -------------------------
+ * heroku create
+ * make sure you: git add. git commit. git push
+ * git push heroku main
+ * Go to heroku DashBoard > Current Project> Settings > Reveal Config verse
+ * copy paste config vars from your .env file
+ * Make sure you have whitelisted all ip address to access mongodb
+ * ------------------------
+ * UPDATE SERVER WITH NEW CHANGES
+ * -------------------------
+ * Make changes
+ * make sure you: git add. git commit. git push
+ * git push heroku main
+ * --------------------------
+ * CONNECT SERVER WITH CLIENT
+ * --------------------------
+ *
+ */
